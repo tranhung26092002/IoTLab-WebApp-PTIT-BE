179e1fe4a91a7bd4e5cf411ccb7d3cee99b64fe9
tranhung26092002 <hungtran2609>
2025-01-08 17:03:38 +0700

    feat: handle fix cors

--- src/main/java/com/ptit/service/config/CorsConfig.java
+++ src/main/java/com/ptit/service/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
       public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://14.225.255.177:3000")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true);  // Quan trọng nếu cần gửi cookies hoặc authorization headers
